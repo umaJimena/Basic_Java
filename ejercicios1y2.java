0b5cdcf3e406156b28cb23b8b9938e65a5741929
Jimena <jimenavaldez0906@gmail.com>
2025-09-25 11:13:04 +0200

    Add files via upload
    primeros ejercicios de Java: variables y conceptos básicos

---
+++ ejercicios1y2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ejercicios1y2 {
+    public static void main(String[] args) {
+        System.out.println("------Ejercicio 1:------>");
+        //Crear un programa que declare una variable de tipo int, 
+        //asignarle el valor 7, 
+        //incrementarle el valor en 1 unidad,
+        //Imprimir el valor.
+        int n1 = 7;
+        System.out.println(++n1);
+
+        System.out.println("------Ejercicio 2:------>");
+        //Crear un programa que declare una variable tipo byte,
+        //asignarle el valor 6,
+        //incrementarle el valor multiplicandolo x2
+        //Imprime el valor y justifica el resultado
+        int n2=65;
+        System.out.println(n2*2);
+        System.out.println("cogemos la variable a la que hemos nombrado n2 y le hemos asignado el valor 65; y la multiplicamos x2 con el simbolo de multiplicacion qu en este caso es * de manera que lo escribiriamos tal que así: n2*2");
+    }
+}
